2dc9223708ea1be62166686c836efdf290ea34bd
Daniela Vargas <lizethdanielavb@ufps.edu.co>
2024-12-02 10:31:07 -0500

    primer commit

---
+++ src/main/java/com/example/examenweb/ExamenwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examenweb/ExamenwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
